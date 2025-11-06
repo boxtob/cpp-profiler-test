8cd12a4b63266f0ae5d773e711370f342fcb5243
Nikolaos Gerontidis <Nikolaos.Gerontidis@gmail.com>
2025-11-07 00:45:08 +0100

    Try to capture the file

--- .github/actions/cpp-profiler/parse_profile.py
+++ .github/actions/cpp-profiler/parse_profile.py
@@ -21,11 +21,11 @@ def parse_valgrind_memcheck(file_path):
         bytes_lost = bytes_lost.replace(",", "")
         print(f"::warning::Valgrind {leak_type}: {bytes_lost} bytes in {blocks} blocks")
 
-    stack_pattern = r"==\d+==\s{4}by 0x[0-9A-F]+: .*?\((.*?):(\d+)\)"
+    stack_pattern = r"==\d+==\s+by 0x[0-9A-F]+: .*?\((.*?):(\d+)\)"
     traces = re.findall(stack_pattern, output, re.MULTILINE)
     for file_name, line_number in traces:
         print(f"::warning::File name {file_name}, line number: {line_number}")
-        if file_name and file_name.startswith("/workspace/"):
+        if file_name and file_name.contains("/workspace/"):
             file_name = file_name.replace("/workspace/", "")
             print(f"::error file={file_name},line={line_number}::Memory leak at {file_name}:{line_number}")
 
