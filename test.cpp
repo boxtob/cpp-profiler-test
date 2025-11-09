63fcc467ab8c36c4b6b33d7e3754acc1eb047769
Nikolaos Gerontidis <Nikolaos.Gerontidis@gmail.com>
2025-11-09 17:30:39 +0100

    Increase stretch

--- test.cpp
+++ test.cpp
@@ -3,8 +3,8 @@
 
 void hot() {
   volatile long sum = 0;
-  for (long i = 0; i < 300'000'000; ++i) sum += i;  // CPU hotspot
-  usleep(100'000);  // Pause for sampling
+  for (long i = 0; i < 2'000'000'000; ++i) sum += i;  // CPU hotspot
+  usleep(200000);  // Pause for sampling
 }
 
 int main() {
